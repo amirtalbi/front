549af5f255bc6ecada772a04477a84dfcb7a9de0
Amir <amirotalbi94@gmail.com>
2023-06-06 12:12:25 +0200

    feat: adding navbar dropdown

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,72 +1,92 @@
-<script setup>
-</script>
 <template>
-<nav class="bg-white navigation">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="#" class="flex items-center">
-      <img src="../assets/logo-dawii.png" class="w-24" alt="Flowbite Logo" >
-    </a>
-    <button data-collapse-toggle="navbar-multi-level" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-multi-level" aria-expanded="false">
-      <span class="sr-only">Open main menu</span>
-      <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-    </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-multi-level">
-      <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
-        <li>
-          <router-link to="/" class="block py-2 pl-3 pr-4 rounded md:bg-transparent md:text-gray-900 md:p-0 md:dark:text-gray-900 dark:text-gray-900 md:dark:bg-transparent">Accueil</router-link>
-        </li>
-        <li>
-          <router-link to="/presentation" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Presentation</router-link>
-        </li>
-        <li>
-          <router-link to="/offres" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Offres</router-link>
-        </li>
-        <li>
-            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4  text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Espaces <svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
-            <!-- Dropdown menu -->
-            <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                  <li>
-                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                  </li>
-                  <li aria-labelledby="dropdownNavbarLink">
-                    <button id="doubleDropdownButton" data-dropdown-toggle="doubleDropdown" data-dropdown-placement="right-start" type="button" class="flex items-center justify-between w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dropdown<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg></button>
-                    <div id="doubleDropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="doubleDropdownButton">
-                          <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Overview</a>
-                          </li>
-                          <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">My downloads</a>
-                          </li>
-                          <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Billing</a>
-                          </li>
-                          <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Rewards</a>
-                          </li>
-                        </ul>
-                    </div>
-                  </li>
-                  <li>
-                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                  </li>
-                </ul>
-                <div class="py-1">
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Sign out</a>
-                </div>
-            </div>
-        </li>
-      </ul>
+  <nav class="bg-white shadow navigation">
+    <div class="max-w-screen-xl mx-auto px-4">
+      <div class="flex items-center justify-between h-32">
+        <!-- Logo -->
+        <div class="flex-shrink-0">
+          <img src="../assets/logo-dawii.png" alt="Logo" class="w-24" />
+        </div>
+
+        <!-- Menu de navigation -->
+        <div class="hidden md:block">
+          <ul class="flex items-center space-x-6">
+            <!-- Éléments de menu -->
+            <li>
+              <a href="#" class="text-black hover:text-gray text-lg">Accueil</a>
+            </li>
+            <li>
+              <a href="#" class="text-black hover:text-gray text-lg"
+                >La formation</a
+              >
+            </li>
+            <!-- Élément de menu dropdown -->
+            <li class="relative" @click="toggleDropdown">
+              <button class="text-black hover:text-gray text-lg">
+                Offres
+                <span class="fa fa-caret-down"></span>
+              </button>
+              <ul
+                v-show="open"
+                class="absolute right-0 mt-2 py-2 w-40 bg-white border border-gray-200 rounded shadow"
+              >
+                <li>
+                  <a
+                    href="#"
+                    class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                    >Projets tutorés</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                    >Alternances</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                    >Stages</a
+                  >
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#" class="text-black hover:text-gray text-lg"
+                >Mon espace</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
-<!-- <script>
+<script>
 export default {
-  mounted()[
-
-  ]
-}
-</script> -->
+  data() {
+    return {
+      open: false,
+    };
+  },
+  mounted() {
+    document.addEventListener("click", this.handleClickOutside);
+  },
+  beforeDestroy() {
+    document.removeEventListener("click", this.handleClickOutside);
+  },
+  methods: {
+    toggleDropdown() {
+      this.open = !this.open;
+    },
+    handleClickOutside(event) {
+      const dropdown = this.$el.querySelector(".relative");
+      if (dropdown && !dropdown.contains(event.target)) {
+        this.open = false;
+      }
+    },
+  },
+};
+</script>
