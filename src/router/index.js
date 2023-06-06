b7f8c996eadbdd35af56b26544cdabe29c780fd9
Amir <amirotalbi94@gmail.com>
2023-06-06 17:19:58 +0200

    fix: setting routes correctly

--- src/router/index.js
+++ src/router/index.js
@@ -4,70 +4,43 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    component: { template: "<router-view></router-view>" },
-    children: [
-      {
-        name: "index",
-        path: "",
-        component: () => import("@/views/IndexView.vue"),
-      },
-      {
-        name: "student-space",
-        path: "/space/:id/student",
-        component: () => import("@/views/StudentSpace.vue"),
-      },
-    ],
-  },
-  {
-    path: "/presentation",
-    component: { template: "<router-view></router-view>" },
-    children: [
-      {
-        name: "presentation",
-        path: "",
-        component: () => import("@/views/PresentationView.vue"),
-      },
-    ],
-  },
-  {
-    path: "/offres",
-    component: { template: "<router-view></router-view>" },
-    children: [
-      {
-        name: "offres",
-        path: "",
-        component: () => import("@/views/OffresView.vue"),
-      },
-    ],
-  },
-  {
-    path: "/espaces",
-    component: { template: "<router-view></router-view>" },
-    children: [
-      {
-        name: "espaces",
-        path: "",
-        component: () => import("@/views/EspacesView.vue"),
-      },
-      {
-        name: "entreprise",
-        path: "entreprise",
-        component: () => import("@/views/Espaces/EntrepriseView.vue"),
-      },
-      {
-        name: "enseignants",
-        path: "enseignants",
-        component: () => import("@/views/Espaces/EnseignantsView.vue"),
-      },
-      {
-        name: "etudiant",
-        path: "etudiant",
-        component: () => import("@/views/Espaces/EtudiantView.vue"),
-      },
-    ],
-  },
+    {
+        path: "/",
+        component: { template: "<router-view></router-view>" },
+        children: [
+            {
+                name: "index",
+                path: "",
+                component: () => import("@/views/IndexView.vue"),
+            },
+            {
+                name: "presentation",
+                path: "la-formation",
+                component: () => import("@/views/PresentationView.vue"),
+            },
+            {
+                path: "espaces/:id",
+                component: { template: "<router-view></router-view>" },
+                children: [
+                    {
+                        name: "entreprise",
+                        path: "entreprise",
+                        component: () => import("@/views/Espaces/EntrepriseView.vue"),
+                    },
+                    {
+                        name: "enseignants",
+                        path: "enseignants",
+                        component: () => import("@/views/Espaces/EnseignantsView.vue"),
+                    },
+                    {
+                        name: "etudiant",
+                        path: "etudiant",
+                        component: () => import("@/views/Espaces/StudentSpace.vue"),
+                    },
+                ],
+            },
+        ],
+    },
 ];
 
 const router = new VueRouter({ routes, mode: "history" });
